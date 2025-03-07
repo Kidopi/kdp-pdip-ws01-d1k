439b49f51f269cd5a707e16f405e039b0addff29
Pedro Henrique Pereira da Silva <88931226+Pedrohenr1qq@users.noreply.github.com>
2025-03-07 18:19:57 +0000

    feat: apresentando informacoes importantes da aplicacao

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-    if($argc > 2){
-        echo "Acao: " . implode(" ", $argv) . PHP_EOL;
-    }
+    $app = "PokeAdventure 0.0.1";
+    $time = date("H:i");
+
+    $trainer = ($argc > 1) ? $argv[1] : "Ash";
+
+    echo "Bem vindo ao programa $app. São exatamente $time e seu treinador de hoje será $trainer" . PHP_EOL;  
 
 ?>

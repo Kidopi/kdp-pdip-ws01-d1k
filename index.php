5b738c9f1d6dad70881871d7e3905ad69825dd70
Pedro Henrique Pereira da Silva <88931226+Pedrohenr1qq@users.noreply.github.com>
2025-03-07 18:40:13 +0000

    feat: criou funcao showMessage

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $app = "PokeAdventure 0.0.1";
-    $time = date("H:i");
-
-    $trainer = ($argc > 1) ? $argv[1] : "Ash";
-
-    echo "Bem vindo ao programa $app." . PHP_EOL. "São exatamente $time." . PHP_EOL . "Seu treinador de hoje será $trainer" . PHP_EOL;  
-
-?>

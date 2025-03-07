d74269f0a4dfaa583e67658e993f77766b4ffd50
Pedro Henrique Pereira da Silva <88931226+Pedrohenr1qq@users.noreply.github.com>
2025-03-07 18:29:34 +0000

    fix: Adicionando quebra de linha nas frases

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 
     $trainer = ($argc > 1) ? $argv[1] : "Ash";
 
-    echo "Bem vindo ao programa $app. São exatamente $time e seu treinador de hoje será $trainer" . PHP_EOL;  
+    echo "Bem vindo ao programa $app." . PHP_EOL. "São exatamente $time." . PHP_EOL . "Seu treinador de hoje será $trainer" . PHP_EOL;  
 
 ?>

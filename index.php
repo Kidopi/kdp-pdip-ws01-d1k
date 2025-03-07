a992d3a96e5daa5c5504c5ed1e034ef2d397abf1
Gabriele Wanzeler <109773605+gabrielewanzeler@users.noreply.github.com>
2025-03-07 18:56:02 +0000

    feature: adiciona função para exibir mensagem

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
+require "./src/Utils.php";
 $nomeAplicacao = "PokeAdventure 0.0.1";
 
 $horario = date('H:i');
 
 $nomeTreinador = isset($argv[1]) ? $argv[1] : 'Treinador desconhecido';
 
-$message = "Bem-vindo ao " . $nomeAplicacao . "!" . PHP_EOL;
-$message .= "Hora atual: " . $horario . PHP_EOL;
-$message .= "Saudações, " . $nomeTreinador . "!" . PHP_EOL;
-
-echo $message;
+showMessage("Bem-vindo ao " . $nomeAplicacao . "!" . PHP_EOL);
+showMessage("Hora atual: " . $horario . PHP_EOL);
+showMessage("Saudações, " . $nomeTreinador . "!" . PHP_EOL);

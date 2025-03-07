c62c03d58a95a44f5970fd1845468f7c4fef480c
Yuri Bertulucci <54747411+yuribertulucci@users.noreply.github.com>
2025-03-07 19:12:35 +0000

    feat: adiciona função gameLoop()

--- index.php
+++ index.php
@@ -5,12 +5,21 @@ require('src/Utils.php');
 
     $appName = "PokeAdventure 0.0.1";
     $time = date("H:i");
-    $trainerName = $argv[1];
+    $trainerName = $argv[1] ?? 'treinador';
 
     showMessage("$appName".PHP_EOL.
     "Hora da execução: $time".PHP_EOL.
     "Bem-vindo, Treinador $trainerName!".PHP_EOL);
 
-    $mensagens = ["Ufa! Treinador ${trainerName}, passamos por matinhos e nada aconteceu.",
-"Eita, prepare-se um Pokemon está bem á sua frente",
-"Olá, gritou uma personagem de longe. Vamos conversar?"];
+    function gameLoop($trainerName): void {
+        $mensagens = ["Ufa! Treinador $trainerName, passamos por matinhos e nada aconteceu.",
+        "Eita, prepare-se um Pokemon está bem á sua frente",
+        "Olá, gritou uma personagem de longe. Vamos conversar?"];
+
+        $randomIndex = rand(0, 2);
+        showMessage($mensagens[$randomIndex]);
+    }
+
+    gameLoop($trainerName);
+
+    

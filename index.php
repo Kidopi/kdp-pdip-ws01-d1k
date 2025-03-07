10fa6ef5e9340b5d3f3256e258aa6eb05f4d5105
Gabriele Wanzeler <109773605+gabrielewanzeler@users.noreply.github.com>
2025-03-07 19:10:18 +0000

    feature: adiciona função para iniciar o jogo

--- index.php
+++ index.php
@@ -10,3 +10,19 @@ $nomeTreinador = isset($argv[1]) ? $argv[1] : 'Treinador desconhecido';
 showMessage("Bem-vindo ao " . $nomeAplicacao . "!" . PHP_EOL);
 showMessage("Hora atual: " . $horario . PHP_EOL);
 showMessage("Saudações, " . $nomeTreinador . "!" . PHP_EOL);
+
+function gameLoop($nomeTreinador) 
+{
+    $valor = random_int(1, 3);
+    if($valor == 1) {
+        showMessage("Ufa! Treinador " .$nomeTreinador. ", passamos por matinhos e nada aconteceu." . PHP_EOL); 
+    }
+    if($valor == 2) {
+        showMessage("Eita, prepare-se um Pokemon está bem á sua frente" . PHP_EOL);
+    }
+    if($valor == 3) {
+        showMessage("Olá, gritou uma personagem de longe. Vamos conversar?" . PHP_EOL);
+    }
+}
+
+gameLoop($nomeTreinador);

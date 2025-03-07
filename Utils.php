3efadc4a30e2d655f9cda8716ca2ed57a4806843
Pedro Henrique Pereira da Silva <88931226+Pedrohenr1qq@users.noreply.github.com>
2025-03-07 19:17:17 +0000

    feat: Adicionando funcao gameLoop e imprimindo mensagem aleatoriamente

--- Utils.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function showMessage($message){
-
-    }
-
-?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 require 'src/Utils.php';
 showMessage('hello world!');
+
+function gameLoop($nomeTreinador){
+    $mensagem = ["Ufa! Treinador $nomeTreinador, passamos por matinhos e nada aconteceu.",
+     "Eita, prepare-se um Pokemon está bem á sua frente",
+      "Olá, gritou uma personagem de longe. Vamos conversar?"];
+
+    echo $mensagem[array_rand($mensagem)] . PHP_EOL;
+}
+
+gameLoop('Pedro');

a268daed5ce5e7de4d3711bc4335c12f465f1395
Pedro Henrique Pereira da Silva <88931226+Pedrohenr1qq@users.noreply.github.com>
2025-03-07 22:47:07 +0000

    fix: continuando a implementacao do sorteio de pokemon

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 require 'src/Utils.php';
-showMessage('hello world!');
 
-function gameLoop($nomeTreinador){
-    $mensagem = ["Ufa! Treinador $nomeTreinador, passamos por matinhos e nada aconteceu.",
-     "Eita, prepare-se um Pokemon está bem á sua frente",
-      "Olá, gritou uma personagem de longe. Vamos conversar?"];
-
-    echo $mensagem[array_rand($mensagem)] . PHP_EOL;
-}
-
-gameLoop('Pedro');
-
-
-// #9 ..revelar qual a especie de monstro que apareceu
 $wildPokemonList = [
     'Pikachu', 
     'Bulbassauro', 
@@ -29,4 +16,16 @@ function wildPokemonAppeared($wildPokemonList){
     return $wildPokemonList[array_rand($wildPokemonList)];
 }
 
-echo wildPokemonAppeared($wildPokemonList) . PHP_EOL;
+function gameLoop($nomeTreinador, $nomePokemon){
+
+    $mensagem = ["Ufa! Treinador $nomeTreinador, passamos por matinhos e nada aconteceu.",
+    "Eita, prepare-se que o $nomePokemon está bem á sua frente",
+    "Olá, gritou uma personagem de longe. Vamos conversar?"];
+
+    echo $mensagem[array_rand($mensagem)] . PHP_EOL;
+}
+
+$nomeTreinador = "Pedro";
+$nomePokemon = wildPokemonAppeared($wildPokemonList);
+
+gameLoop($nomeTreinador, $nomePokemon);
